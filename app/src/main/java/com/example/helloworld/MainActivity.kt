42b6f1249a90bdbe56be778752c02db9bc93625c
Sanskar Gupta <sanskargupta1404@gmail.com>
2024-06-08 10:29:44 +0530

    button-click

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickHandler(view: View) {
+        Log.i(tag: "MainActivity", msg: "button Clicked")
     }
 }
